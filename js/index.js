1c8c0464fdd74dce783f21d147af0fd42506aac3
WAO-Z <576816951@qq.com>
2021-06-09 23:30:35 +0800

    third

--- js/index.js
+++ js/index.js
@@ -4,104 +4,130 @@ let pointList = document.querySelectorAll(".pointBox li");
 let ul = document.querySelector(".carouselBox ul");
 let carouselBox = document.querySelector(".carouselBox");
 let nowIndex = 0;
+let timer;
+
+/**
+ * 左箭头事件
+ */
+leftArrow.addEventListener("click", function () {
+
+	if (++nowIndex == ul.childElementCount) {
+		leftEndRun();
+	} else {
+		run();
+	}
+
+})
+
+/**
+ * 左边尽头函数->这一张去到第一张
+ */
+function leftEndRun() {
+	ul.appendChild(ul.firstElementChild.cloneNode(true));
+	ul.style.transform = `translateX(-${nowIndex}00%)`;
+
+	ul.addEventListener("transitionend", function () {
+		ul.style.transition = "none"
+		ul.style.transform = `translateX(0%)`;
+
+		//读取ul的transition属性 提醒代码运行时注意样式改变的能力
+		getComputedStyle(ul).transition;
+		ul.style.transition = "";
+
+		ul.removeChild(ul.lastElementChild);
+		nowIndex = 0
+	}, {
+		once: true
+	})
+
+	pointList[2].classList.remove("active")
+	pointList[0].classList.add("active");
+
+
+}
 
 /**
  * 右箭头事件
  */
 rightArrow.addEventListener("click", function () {
 
-	// nowIndex++;
+	nowIndex--
+	if (nowIndex == -1) {
+		ul.appendChild(ul.firstElementChild.cloneNode(true));
+		ul.style.transition = "none"
+		ul.style.transform = `translateX(-300%)`;
+		nowIndex = 2
 
-	// if(nowIndex == ul.childElementCount){
-	// 	nowIndex = 0
-	// }
+		getComputedStyle(ul).transition;
+		ul.style.transition = ""
+		ul.style.transform = `translateX(-200%)`;
 
-	(++nowIndex == ul.childElementCount) && (nowIndex = 0)
+		pointList[0].classList.remove("active")
+		pointList[2].classList.add("active");
+		setTimeout(function () {
+			ul.removeChild(ul.lastElementChild);
+		}, 300)
+	} else {
+		run()
+	}
+})
 
-	ul.style.transform = `translateX(-${nowIndex}00%)`;
-	pointList.forEach(function (item) {
-		item.classList = ""
+
+/**
+ * 小圆点的点击事件
+ */
+pointList.forEach(function (item, index) {
+	item.addEventListener("click", function () {
+		nowIndex = index;
+		run();
 	})
-
-	pointList[nowIndex].classList = "active"
 })
 
 /**
- * 左箭头事件
+ * 自动轮播
  */
-leftArrow.addEventListener("click", function () {
-	// nowIndex--;
+timer = setInterval(function () {
+	if (++nowIndex == ul.childElementCount) {
+		leftEndRun();
+	} else {
+		run();
+	}
 
-	// if (nowIndex < 0) {
-	// 	nowIndex = ul.childElementCount - 1;
-	// }
+}, 2000)
 
-	(--nowIndex < 0) && (nowIndex = ul.childElementCount - 1)
-
-	ul.style.transform = `translateX(-${nowIndex}00%)`;
-
-	pointList.forEach(function (item) {
-		item.classList = ""
-	})
-
-	pointList[nowIndex].classList = "active"
-})
-
-//小圆点点击事件
-pointList[2].addEventListener("click", function () {
-
-	ul.style.transform = `translateX(-200%)`;
-	pointList.forEach(function (item) {
-		item.classList = ""
-	})
-	pointList[2].classList = "active"
-})
-pointList[1].addEventListener("click", function () {
-
-	ul.style.transform = `translateX(-100%)`;
-	pointList.forEach(function (item) {
-		item.classList = ""
-	})
-	pointList[1].classList = "active"
-})
-pointList[0].addEventListener("click", function () {
-
-	ul.style.transform = `translateX(0%)`;
-	pointList.forEach(function (item) {
-		item.classList = ""
-	})
-	pointList[0].classList = "active"
-})
-
-//自动轮播
-let auto = setInterval(function () {
-	(++nowIndex == ul.childElementCount) && (nowIndex = 0)
-
-	ul.style.transform = `translateX(-${nowIndex}00%)`;
-	pointList.forEach(function (item) {
-		item.classList = ""
-	})
-	pointList[nowIndex].classList = "active"
-
-}, 1000)
-
-//取消轮播
+/**
+ * 鼠标移入事件
+ */
 carouselBox.addEventListener("mouseenter", function () {
-	clearInterval(auto)
+	clearInterval(timer);
 })
 
-//添加轮播
+/**
+ * 鼠标移出事件
+ */
 carouselBox.addEventListener("mouseleave", function () {
-	auto = setInterval(function () {
-		(++nowIndex == ul.childElementCount) && (nowIndex = 0)
+	timer = setInterval(function () {
 
-		ul.style.transform = `translateX(-${nowIndex}00%)`;
-		pointList.forEach(function (item) {
-			item.classList = ""
-		})
-		pointList[nowIndex].classList = "active"
 
-	}, 1000)
+		if (++nowIndex == ul.childElementCount) {
+			leftEndRun();
+		} else {
+			run();
+		}
+
+	}, 2000)
 })
 
+/**
+ * 运行的主函数
+ */
+function run() {
+	ul.style.transform = `translateX(-${nowIndex}00%)`;
 
+	pointList.forEach(function (item) {
+		item.classList.remove("active");
+	})
+
+	pointList[nowIndex].classList.add("active");
+
+}
